c0e6134e6fcb003704d480c01a8bb82762bd6748
Krushna <krushnath.dhongade@alignedautomation.com>
2023-07-24 17:04:38 +0530

    Added final test for broken images

--- tests/Elements.spec.js
+++ tests/Elements.spec.js
@@ -281,9 +281,7 @@ test("Table Delete button Functionality", async ({ page }) => {
     .allTextContents();
   afterTableData = afterTableData.filter((item) => item.trim() !== "");
   afterTableData.shift();
-  console.log("After new row add: ",afterTableData);
-
-  
+  console.log("After new row add: ", afterTableData);
 
   // finding out the element row number with name Test to be Deleted
 
@@ -310,20 +308,21 @@ test("Table Delete button Functionality", async ({ page }) => {
       "//div[@class='ReactTable -striped -highlight']//div[@role='row']/div[1]"
     )
     .allTextContents();
-  afterDeleteTableData = afterDeleteTableData.filter((item) => item.trim() !== "");
+  afterDeleteTableData = afterDeleteTableData.filter(
+    (item) => item.trim() !== ""
+  );
   afterDeleteTableData.shift();
-  console.log("After Delete row added: ",afterDeleteTableData);
+  console.log("After Delete row added: ", afterDeleteTableData);
 
   expect(afterDeleteTableData.includes("Test")).toBeFalsy();
-  expect(afterTableData.length-1 === afterDeleteTableData.length).toBeTruthy();
-
-
+  expect(
+    afterTableData.length - 1 === afterDeleteTableData.length
+  ).toBeTruthy();
 });
 
 //////// Test and examine the table serach functionality  /////////////////////
 
-test('Table Search Test', async({page}) =>{
-
+test("Table Search Test", async ({ page }) => {
   await page.goto("https://demoqa.com/webtables");
 
   await expect(page.locator(".main-header")).toHaveText("Web Tables");
@@ -367,137 +366,230 @@ test('Table Search Test', async({page}) =>{
     .allTextContents();
   afterTableData = afterTableData.filter((item) => item.trim() !== "");
   afterTableData.shift();
-  console.log("After new row add: ",afterTableData);
-///starting search 
+  console.log("After new row add: ", afterTableData);
+  ///starting search
 
-  await page.locator("//input[@id='searchBox']").type("test",{delay :200});
-  let afterSearchData = await page.locator("//div[@class='ReactTable -striped -highlight']//div[@role='row']/div[1]").allTextContents();
+  await page.locator("//input[@id='searchBox']").type("test", { delay: 200 });
+  let afterSearchData = await page
+    .locator(
+      "//div[@class='ReactTable -striped -highlight']//div[@role='row']/div[1]"
+    )
+    .allTextContents();
   afterSearchData = afterSearchData.filter((item) => item.trim() !== "");
   afterSearchData.shift();
-  console.log("After search : ",afterSearchData);
+  console.log("After search : ", afterSearchData);
   let flag = false;
-  afterSearchData.forEach(element => {
-    
-    if(element.toLowerCase().includes("test")){
-      flag= true;
-    }else{
-      flag =false;
+  afterSearchData.forEach((element) => {
+    if (element.toLowerCase().includes("test")) {
+      flag = true;
+    } else {
+      flag = false;
     }
-    
   });
   expect(flag).toBeTruthy();
-
-
 });
 
 /////// Test and Examine the buttons different clicks functionality  ////////////////////
 
-test('Button Clicks Test', async({page}) =>{
-
+test("Button Clicks Test", async ({ page }) => {
   await page.goto("https://demoqa.com/buttons");
 
   await expect(page.locator(".main-header")).toHaveText("Buttons");
-/// checking the doubleClcik functionality.
+  /// checking the doubleClcik functionality.
   await page.locator("//button[@id='doubleClickBtn']").dblclick();
 
   //checking the message appearred or not
-   expect((await page.locator("//p[@id='doubleClickMessage']").allTextContents())).toContain("You have done a double click");
+  expect(
+    await page.locator("//p[@id='doubleClickMessage']").allTextContents()
+  ).toContain("You have done a double click");
 
-   //cheking right click functionality
+  //cheking right click functionality
 
-   await page.locator("//button[@id='rightClickBtn']").click({button: "right"});
-   expect(await page.locator("//p[@id='rightClickMessage']").allTextContents()).toContain("You have done a right click");
+  await page
+    .locator("//button[@id='rightClickBtn']")
+    .click({ button: "right" });
+  expect(
+    await page.locator("//p[@id='rightClickMessage']").allTextContents()
+  ).toContain("You have done a right click");
 
   //Dynamic id buttons  click functionality check
-    // const buttonText = "Click Me";
-    // await page.locator(`button:has-text("${buttonText}")`).nth(2).click();  or simple is below
-    await page.locator("//button[@class='btn btn-primary']").nth(2).click(); // as it is third button
-    // you can use nth of last too as it is las button too.
-    
-    expect(await page.locator("//p[@id='dynamicClickMessage']").allTextContents()).toContain("You have done a dynamic click");
+  // const buttonText = "Click Me";
+  // await page.locator(`button:has-text("${buttonText}")`).nth(2).click();  or simple is below
+  await page.locator("//button[@class='btn btn-primary']").nth(2).click(); // as it is third button
+  // you can use nth of last too as it is las button too.
 
-
+  expect(
+    await page.locator("//p[@id='dynamicClickMessage']").allTextContents()
+  ).toContain("You have done a dynamic click");
 });
-
 
 ////////////////////// Test and Verify the links there functionality //////////////////
 
-
-test.only('Test the different links functionality', async({page}) => {
-
+test("Test the different links functionality", async ({ page }) => {
   await page.goto("https://demoqa.com/links");
 
   await expect(page.locator(".main-header")).toHaveText("Links");
-  
-  // tesing simple link that opens new tab 
+
+  // tesing simple link that opens new tab
   // Get the current window.
-  const currentPage =  page;
+  const currentPage = page;
 
   // Get the promise for the popup event.
-  const page1Promise = page.waitForEvent('popup');
+  const page1Promise = page.waitForEvent("popup");
   // now clicking on the desired link
   await page.locator("//a[@id='simpleLink']").click();
   // Get the popup window.
   const page1 = await page1Promise;
-  
+
   //checking if the new window tab opened
   let flag = false;
-  if(currentPage !== page1){
+  if (currentPage !== page1) {
     flag = true;
-  }else{
+  } else {
     flag = false;
   }
   expect(flag).toBeTruthy();
-  console.log(currentPage.url()," and ", page1.url());
+  console.log(currentPage.url(), " and ", page1.url());
 
-  /// checking links that sends the API call 
-  
+  /// checking links that sends the API call
+
   // intercept the api call
-  await page.route('**/created',(route) => {
-
+  await page.route("**/created", (route) => {
     route.fulfill({
-      status :201,
+      status: 201,
       // @ts-ignore
-      statusText :'Created',
+      statusText: "Created",
     });
   });
 
   await page.locator("//a[@id='created']").click();
-  const messageText = await page.locator("//p[@id='linkResponse']").textContent();
-  expect(messageText).toContain("Link has responded with staus 201 and status text Created");
-  // next link to test  
+  const messageText = await page
+    .locator("//p[@id='linkResponse']")
+    .textContent();
+  expect(messageText).toContain(
+    "Link has responded with staus 201 and status text Created"
+  );
+  // next link to test
   /// below part is neccesory for the creating simulated responce that actual site gives. needed for playwright.
-  await page.route('**/no-content',(route) => {
-
+  await page.route("**/no-content", (route) => {
     route.fulfill({
-      status : 204, 
+      status: 204,
       // @ts-ignore
-      statusText :'No Content',
-
-
+      statusText: "No Content",
     });
   });
 
   await page.locator("//a[@id='no-content']").click();
   let message = await page.locator("//p[@id='linkResponse']").textContent();
-  expect(message).toContain("Link has responded with staus 204 and status text No Content");
- 
+  expect(message).toContain(
+    "Link has responded with staus 204 and status text No Content"
+  );
+
   /// next link to be checked:
-  await page.route('**/moved',(route) => {
+  await page.route("**/moved", (route) => {
     route.fulfill({
-      status :301,
+      status: 301,
       // @ts-ignore
-      statusText : 'Moved Permanently',
-
+      statusText: "Moved Permanently",
     });
-
   });
 
   await page.locator("//a[@id='moved']").click();
-  expect(await page.locator("//p[@id='linkResponse']").textContent()).toContain("Link has responded with staus 301 and status text Moved Permanently");
-
+  expect(await page.locator("//p[@id='linkResponse']").textContent()).toContain(
+    "Link has responded with staus 301 and status text Moved Permanently"
+  );
 });
 
+//////////////////////////// test and verify Broken link - Images ////////////////////////////
 
+test.only("Test Broken links and Image", async ({ page }) => {
+  await page.goto("https://demoqa.com/broken");
 
+  // checking we are on right page
+  await expect(page.locator(".main-header")).toHaveText(
+    "Broken Links - Images"
+  );
+  const allImages = await page.locator("img");
 
+  // Extract the src attribute from each image element
+  const imageSrcs = await allImages.evaluateAll((elements) => {
+    return elements.map((element) => element.getAttribute("src"));
+  });
+
+  console.log(imageSrcs);
+  // now we are removing the imgages like advertising images
+  const filteredImageSrcs = imageSrcs.filter(
+    (element) => !element.includes("ad")
+  );
+  console.log(filteredImageSrcs);
+
+  // we are creating the final url array that we will check one by one for all url
+
+  const baseUrl = "https://demoqa.com";
+  const finalUrls = filteredImageSrcs.map((url) => baseUrl + url);
+  console.log(finalUrls);
+  
+
+  //checking if every source of the image working properly by opeing src in new tab
+  const imageResult = [];
+  // Open a new tab for each image source URL
+  for (const element of finalUrls) {
+    let newPage = await page.context().newPage();
+
+    await newPage.goto(element);
+
+    let newPageImage = await newPage.locator("img");
+
+    // Extract the src attribute from each image element for new page
+    let newPageSrc = await newPageImage.evaluateAll((item) => {
+      return item.map((item) => item.getAttribute("src"));
+    });
+    let newPageImageSrcs = newPageSrc.filter(
+      (element) => !element.includes("ad")
+    );
+    console.log("New page", newPageImageSrcs);
+    // checking that image src is present in the new tab
+
+    if (newPageImageSrcs.some((src) => element.includes(src))) {
+      imageResult.push("true");
+      console.log(element, " is valid image");
+    } else {
+      imageResult.push("false");
+      console.log(element, " is broken image");
+    }
+
+    await newPage.close();
+  }
+  console.log(imageResult);
+  // Adding assertion on the all imges
+  imageResult.forEach((element) => {
+    expect(element).toEqual('true');
+  });
+});
+
+////////////////// Check and Verify the Download and upload functionality ///////////////
+
+test("Test Download and upload button", async ({ page, context }) => {
+  await page.goto("https://demoqa.com/upload-download");
+
+  await expect(page.locator(".main-header")).toHaveText("Upload and Download");
+
+  await page.locator("//a[@id='downloadButton']").click();
+
+  // Get the downloaded file path
+  const downloads = await page.waitForEvent("download");
+  const download = downloads[0]; // Assuming there's only one download
+  const filePath = download.path();
+
+  // Check if the file exists
+  const fs = require("fs");
+  const fileExists = fs.existsSync(filePath);
+  expect(fileExists).toBeTruthy();
+
+  // Optionally, you can also check the file size or other properties
+  const fileSize = fs.statSync(filePath).size;
+  expect(fileSize).toBeGreaterThan(0);
+
+  // Clean up: remove the downloaded file after the test is complete
+  fs.unlinkSync(filePath);
+});
